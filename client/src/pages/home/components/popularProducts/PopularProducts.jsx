0b7e81fd80c3d9ac3b65222c4824ce1d86dc3f03
TahmidShawn7 <md.tahmid022@gmail.com>
2026-07-18 05:38:19 +0600

    fix: solve popular product card import name issue

--- client/src/pages/home/components/popularProducts/PopularProducts.jsx
+++ client/src/pages/home/components/popularProducts/PopularProducts.jsx
@@ -1,5 +1,5 @@
 import SectionTitle from "../../../../components/shared/sectionTitle/SectionTitle";
-import PopularProductCard from "./popularProductCard";
+import PopularProductCard from "./PopularProductCard";
 import { popularProducts } from "../../../../data/data";
 
 const PopularProducts = () => {
