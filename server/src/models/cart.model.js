c24119da1415b632217e997457ae345032f61780
TahmidShawn7 <md.tahmid022@gmail.com>
2026-08-09 17:40:24 +0600

    feat: design cart model

---
+++ server/src/models/cart.model.js
@@ -0,0 +1,30 @@
+const cartSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+            unique: true,
+        },
+        items: [
+            {
+                product: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "Product",
+                    required: true,
+                },
+                quantity: {
+                    type: Number,
+                    required: true,
+                    min: [1, "Quantity cannot be less than 1"],
+                    default: 1,
+                },
+                _id: false,
+            },
+        ],
+    },
+    { timestamps: true },
+);
+
+const Cart = mongoose.models.Cart || mongoose.model("Cart", cartSchema);
+export default Cart;
